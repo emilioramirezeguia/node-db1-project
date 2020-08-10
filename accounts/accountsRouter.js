13996999d9eba08f732dfbf397738d07b0e607b8
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-08-10 15:53:54 -0700

    GET all accounts using knex

---
+++ accounts/accountsRouter.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const db = require("../data/dbConfig.js");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  db.select("*")
+    .from("accounts")
+    .then((accounts) => {
+      res.status(200).json({ accounts: accounts });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
-
 const db = require("../data/dbConfig.js");
-
 const server = express();
-
+const accountsRouter = require("../accounts/accountsRouter");
 server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "It's working... it's working!" });
+});
+
+server.use("/accounts", accountsRouter);
 
 module.exports = server;
