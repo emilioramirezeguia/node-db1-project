187684f6cbfa7f16570a346dfd7d1da931f98a81
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-08-10 16:05:03 -0700

    POST new account using knex

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -3,11 +3,28 @@ const db = require("../data/dbConfig.js");
 
 const router = express.Router();
 
+// GET all accounts
 router.get("/", (req, res) => {
   db.select("*")
     .from("accounts")
     .then((accounts) => {
       res.status(200).json({ accounts: accounts });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
+
+// Create (POST) a new account
+router.post("/", (req, res) => {
+  const post = req.body;
+
+  db.select("*")
+    .from("accounts")
+    .insert(post)
+    .returning("id")
+    .then((ids) => {
+      res.status(201).json({ inserted: ids });
     })
     .catch((error) => {
       res.status(500).json({ error: error.message });
